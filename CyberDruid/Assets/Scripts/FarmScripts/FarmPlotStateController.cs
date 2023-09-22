ef330e2bdd45d2dc0dcc050847a9505d8b573cf9
egray <evangray7373@gmail.com>
2023-09-21 22:33:43 -0700

    Added farm-player interaction: TILL, PLANT, HARVEST

--- CyberDruid/Assets/Scripts/FarmScripts/FarmPlotStateController.cs
+++ CyberDruid/Assets/Scripts/FarmScripts/FarmPlotStateController.cs
@@ -25,8 +25,7 @@ public class FarmPlotStateController : MonoBehaviour
 
     public void Start()
     {
-        Debug.Log("Press 'C' to create a test farm region with 6 nonarable farm plots.");
-        Debug.Log("(After pressing 'C') Press 'X' to convert a nonarable farm plot to an arable farm plot.");
+        Debug.Log("TEST FARM SETUP:\n\n\n1. Press 'C' to create a test farm region with 6 nonarable farm plots.\n2. (After pressing 'C') Press 'X' to convert a nonarable farm plot to an arable farm plot.\n\n\nPLAYER-FARMPLOT INTERACTIONS:\n\n\n1. When standing on an arable farm plot, press 'T' to till the land.\n2. When standing on a tilled farm plot, press 'P' to plant a seed on the farm plot.\n3. To progress plant growth on all sown farm plots, press 'Z'.\n4. When standing on a farm plot with a fully grown plant, press 'H' to harvest and clear the farm plot.");
 
         FarmPlots = new Dictionary<GameObject, List<FarmPlot>>();
 
@@ -35,6 +34,7 @@ public class FarmPlotStateController : MonoBehaviour
 
     public void Update()
     {
+        /* TEST FARM SETUP */
         // Test input for initializing farm functionality test
         if (Input.GetKeyDown(KeyCode.C))
         {
@@ -51,17 +51,99 @@ public class FarmPlotStateController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.X) && FarmRegions.Count > 0)
         {
             Debug.Log("Acquire arable land test key pressed...");
-            AcquireArableFarmPlot_Test(FarmRegions.First());
+            AcquireArableFarmPlot(FarmRegions.First());
         }
 
-        if (Input.GetKeyDown(KeyCode.F))
+        // Test input for progressing plant growth
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            TillArableFarmPlot_Test();
+            DEBUG_ProgressCropGrowth();
+        }
+
+
+        /* PLAYER FARM INTERACTIONS */
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TillFarmPlot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlantSeed();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            HarvestPlant();
         }
     }
 
+    public void DEBUG_ProgressCropGrowth()
+    {
+        List<FarmPlot> farmPlots = FarmPlots[FarmRegions.First()];
+
+        foreach (FarmPlot farmPlot in farmPlots)
+        {
+            if (farmPlot.State == FarmPlotState.Sown)
+            {
+                farmPlot.State = FarmPlotState.Grown;
+                Debug.Log("DEBUG COMMAND: Progressed plant growth. Sown plots are now ready for harvest.");
+            }
+        }
+    }
+
+    // Plant a seed on a tilled farm plot. Changes plot state from Grown > Arable.
+    // Note: Harvest plant restarts the state cycle of the farm plot.
+    public void HarvestPlant()
+    {
+        FarmPlot targetFarmPlot = GetTargetFarmPlot(FarmPlotState.Grown);
+
+        if (targetFarmPlot != null)
+        {
+            // Include logic to interact with player inventory here
+            // if (plant gives weapon) { give player weapon } ... etc.
+            targetFarmPlot.State = FarmPlotState.Arable;
+            Debug.Log("Plant harvested from farm plot at location (" + targetFarmPlot.Location.x + ", " + targetFarmPlot.Location.y + ").");
+        }
+        else
+            Debug.Log("Player is not standing on a farm plot with a fully grown plant. There is no plant to harvest here. I am deeply upset.");
+    }
+
+    // Plant a seed on a tilled farm plot. Changes plot state from Tilled > Sown
+    // Note: The transition from Sown > Grown is undecided. We are considering hinging
+    //       plant growth on the amount of dungeon runs there are.
+    public void PlantSeed()
+    {
+        FarmPlot targetFarmPlot = GetTargetFarmPlot(FarmPlotState.Tilled);
+
+        if (targetFarmPlot != null)
+        {
+            // Include logic to interact with player inventory here
+            // if (player has seeds) { ... } ... etc.
+            targetFarmPlot.State = FarmPlotState.Sown;
+            Debug.Log("Seed planted on farm plot at location (" + targetFarmPlot.Location.x + ", " + targetFarmPlot.Location.y + ").");
+        }
+
+        else
+            Debug.Log("Player is not standing on a tilled farm plot. Nowhere to plant my awesome seeds. This is terrible.");
+    }
+
     // Till a farm plot. Changes plot state from Arable > Tilled
-    public void TillArableFarmPlot_Test()
+    public void TillFarmPlot()
+    {
+        FarmPlot targetFarmPlot = GetTargetFarmPlot(FarmPlotState.Arable);
+
+        if (targetFarmPlot != null)
+        {
+            targetFarmPlot.State = FarmPlotState.Tilled;
+            Debug.Log("Tile at location (" + targetFarmPlot.Location.x + ", " + targetFarmPlot.Location.y + ") tilled.");
+        }
+
+        else
+            Debug.Log("Player is not standing on an arable farm plot. Nothing to till here!");
+    }
+
+    FarmPlot GetTargetFarmPlot(FarmPlotState searchState)
     {
         // If player is standing on farm tile
         List<FarmPlot> farmPlots = FarmPlots[FarmRegions.First()];
@@ -69,25 +151,21 @@ public class FarmPlotStateController : MonoBehaviour
         FarmPlot targetFarmPlot = null;
         foreach (FarmPlot farmPlot in farmPlots)
         {
-            if (MapTileManager.PlayerCurrentTile == farmPlot.Location && farmPlot.State == FarmPlotState.Arable)
+            if (MapTileManager.PlayerCurrentTile == farmPlot.Location && farmPlot.State == searchState)
             {
+                // Player is standing on the target farm plot, and the farm plot state is appropriate
                 targetFarmPlot = farmPlot;
                 break;   
             }
         }
 
-        if (targetFarmPlot != null)
-        {
-            targetFarmPlot.State = FarmPlotState.Tilled;
-            Debug.Log("Tile at location (" + targetFarmPlot.Location.x + ", " + targetFarmPlot.Location.y + ") tilled.");
-        }
-        else
-            Debug.Log("Player is not standing on an arable farm plot. Nothing to till here!");
+        return targetFarmPlot;
     }
 
     // Convert a farm plot. Changes plot state from Nonarable > Arable
     // Note: The method and process of farm expansion can be decided later.
-    public void AcquireArableFarmPlot_Test(GameObject farmRegion)
+    //       Currently, tile states from the test hardcoded farm region are randomly swapped
+    public void AcquireArableFarmPlot(GameObject farmRegion)
     {
         List<FarmPlot> nonArableFarmPlots = new List<FarmPlot>();
         foreach (FarmPlot farmPlot in FarmPlots[farmRegion])
@@ -110,6 +188,7 @@ public class FarmPlotStateController : MonoBehaviour
         Debug.Log("Farm plot at location (" + selectedFarmPlot.Location.x + ", " + selectedFarmPlot.Location.y + ") is now arable.");
     }
 
+    // Initialize game objects to track farm regions + farm tiles and state
     void InitializeFarm_Test()
     {
         List<Vector3Int> testFarmPlotCoordinates = new List<Vector3Int>();
