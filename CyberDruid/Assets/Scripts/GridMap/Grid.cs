f648d7be374d055966304ca9e6fe6f0a53b07bb2
Reid Trisler <reidtrisler@yahoo.com>
2023-10-01 22:23:43 -0500

    Implementing A* Pathfinding

--- CyberDruid/Assets/Scripts/GridMap/Grid.cs
+++ CyberDruid/Assets/Scripts/GridMap/Grid.cs
@@ -38,7 +38,7 @@ public class Grid<TGridObject> {
 
             for (int x = 0; x < gridArray.GetLength(0); x++) {
                 for (int y = 0; y < gridArray.GetLength(1); y++) {
-                    debugTextArray[x, y] = CreateWorldText(gridArray[x, y]?.ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 30, Color.white, TextAnchor.MiddleCenter);
+                    debugTextArray[x, y] = CreateWorldText(gridArray[x, y]?.ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 500, 0.01f, Color.white, TextAnchor.MiddleCenter);
                     Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                     Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
                 }
@@ -68,7 +68,7 @@ public class Grid<TGridObject> {
         return new Vector3(x, y) * cellSize + originPosition;
     }
 
-    private void GetXY(Vector3 worldPosition, out int x, out int y) {
+    public void GetXY(Vector3 worldPosition, out int x, out int y) {
         x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
         y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
     }
@@ -104,13 +104,13 @@ public class Grid<TGridObject> {
         return GetGridObject(x, y);
     }
 
-    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault) {
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, float characterSize = 0.01f, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault) {
             if (color == null) color = Color.white;
-            return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+            return CreateWorldText(parent, text, localPosition, fontSize, characterSize, (Color)color, textAnchor, textAlignment, sortingOrder);
         }
         
         // Create Text in the World
-        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder) {
+        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, float characterSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder) {
             GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
             Transform transform = gameObject.transform;
             transform.SetParent(parent, false);
@@ -120,6 +120,7 @@ public class Grid<TGridObject> {
             textMesh.alignment = textAlignment;
             textMesh.text = text;
             textMesh.fontSize = fontSize;
+            textMesh.characterSize = characterSize;
             textMesh.color = color;
             textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
             return textMesh;
--- CyberDruid/Assets/Scripts/PathNode.cs
+++ CyberDruid/Assets/Scripts/PathNode.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class PathNode
 {
-   //private Grid<PathNode> grid;
-   private int x, y;
+   private Grid<PathNode> grid;
+   public int x, y;
 
     public int gCost;
     public int hCost;
     public int fCost;
+    public bool isWalkable;
 
     public PathNode cameFromNode;
 
-   /*public PathNode(Grid<PathNode> grid, int x, int y)
+   public PathNode(Grid<PathNode> grid, int x, int y)
    {
         this.grid = grid;
         this.x = x;
         this.y = y;
-   } */
+   }
+
+   public void CalculateFCost()
+   {
+        fCost = gCost + hCost;
+   }
+
+    public void SetIsWalkable(bool isWalkable) {
+        this.isWalkable = isWalkable;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
 
     public override string ToString(){
         return x + ", " + y;
