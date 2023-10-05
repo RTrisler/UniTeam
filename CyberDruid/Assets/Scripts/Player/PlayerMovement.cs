624cfd72919a1230bf0f1ee71c13b27ce016f8c5
Reid Trisler <reidtrisler@yahoo.com>
2023-10-05 16:53:37 -0500

    Dash Changes
    Dashing now uses physics instead of just translating the transform. Also added water animation.

--- CyberDruid/Assets/Scripts/Player/PlayerMovement.cs
+++ CyberDruid/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,20 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 targetPos;
+    #region Public Variables
     public float moveSpeed;
-    public Rigidbody2D rb;
-    public float dashRange;
+    public float dashAmount = 15f;
 
-    private Vector2 moveDirection;
+    #endregion 
+
+    #region Private Variables
+    
+    private Rigidbody2D rb;
     private Animator animator;
+    private Vector2 moveDirection;
+    private Vector2 targetPos;
+    private bool isDashing = false;
+    [SerializeField] private LayerMask dashLayerMask;
+    
+
+    #endregion
     
     // Assign the actions asset to this field in the inspector:
     private GlobalInputActions Actions;
 
     // Determine what direction player is facing
     private enum Facing {UP, DOWN, LEFT, RIGHT};
-    private Facing facingDir = Facing.DOWN;
+    private Facing facingDir = Facing.UP;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +38,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
+        // Get Rigidbody
+        rb = GetComponent<Rigidbody2D>();
+
         Actions = new GlobalInputActions();
         Actions.Player.Dash.performed += OnDash;
     }
@@ -44,6 +57,7 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    // Determine the inputs and put them in a vector to be used by Fixed Update processes
     void ProcessInputs()
     {
         Vector2 moveVector = Actions.Player.Move.ReadValue<Vector2>();
@@ -59,27 +73,44 @@ public class PlayerMovement : MonoBehaviour
         else if (moveDirection.x == 0f & moveDirection.y == -1f)
             facingDir = Facing.DOWN;
     }
+    void Move()
+    {
+        // Add velocity to the player rigidbody based on the moveDirection Vector
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        setAnimatorMovement(moveDirection);
+
+        if (isDashing)
+        { 
+            Vector2 myPos = transform.position;
+            Vector2 dashPosition = myPos + moveDirection * dashAmount;
+
+            RaycastHit2D raycastHit2d = Physics2D.Raycast(transform.position, moveDirection, dashAmount, dashLayerMask);
+            if (raycastHit2d.collider != null)
+            {
+                dashPosition = raycastHit2d.point;
+            }
+
+            rb.MovePosition(dashPosition);
+            isDashing = false;
+        }
+    }
 
     private void OnDash(InputAction.CallbackContext context)
     {
-        targetPos = Vector2.zero;
+        isDashing = true;
 
-        if (facingDir == Facing.UP)
-            targetPos.y = 1;
-        if (facingDir == Facing.DOWN)
-            targetPos.y = -1;
-        if (facingDir == Facing.LEFT)
-            targetPos.x = -1;
-        if (facingDir == Facing.RIGHT)
-            targetPos.x = 1;
+        // targetPos = Vector2.zero;
 
-        transform.Translate(targetPos * dashRange);
-    }
+        // if (facingDir == Facing.UP)
+        //     targetPos.y = 1;
+        // if (facingDir == Facing.DOWN)
+        //     targetPos.y = -1;
+        // if (facingDir == Facing.LEFT)
+        //     targetPos.x = -1;
+        // if (facingDir == Facing.RIGHT)
+        //     targetPos.x = 1;
 
-    void Move()
-    {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-        setAnimatorMovement(moveDirection);
+        // transform.Translate(targetPos * dashRange);
     }
 
     private void setAnimatorMovement(Vector2 direction)
