22cf47c89930a1c38e7e4b1ea6172a14075a2d44
Reid Trisler <reidtrisler@yahoo.com>
2023-11-14 16:29:23 -0600

    More Progress on Dungeons

--- CyberDruid/Assets/Scripts/ProcuderalGeneration/TilemapVisualizer.cs
+++ CyberDruid/Assets/Scripts/ProcuderalGeneration/TilemapVisualizer.cs
@@ -7,9 +7,9 @@ using UnityEngine.Tilemaps;
 public class TilemapVisualizer : MonoBehaviour
 {
     [SerializeField]
-    private Tilemap floorTilemap;
+    private Tilemap floorTilemap, wallTilemap;
     [SerializeField]
-    private TileBase floorTile;
+    private TileBase floorTile, wallTop;
 
     public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
     {
@@ -29,4 +29,15 @@ public class TilemapVisualizer : MonoBehaviour
         var tilePosition = tilemap.WorldToCell((Vector3Int)position);
         tilemap.SetTile(tilePosition, tile);
     }
+
+    internal void PaintSingleBasicWall(Vector2Int position)
+    {
+        PaintSingleTile(wallTilemap, wallTop, position);
+    }
+    public void Clear()
+    {
+        floorTilemap.ClearAllTiles();
+        wallTilemap.ClearAllTiles();
+    }
+
 }
