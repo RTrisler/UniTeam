a44541b7a21e64016309214700d87a0b38e41401
egray <63073663+iggy808@users.noreply.github.com>
2024-01-09 19:04:16 -0600

    Added sprites to farm states

--- CyberDruid/Assets/Scripts/FarmScripts/FarmInteractions.cs
+++ CyberDruid/Assets/Scripts/FarmScripts/FarmInteractions.cs
@@ -24,13 +24,13 @@ public class FarmInteractions : MonoBehaviour
 
     private void OnFarmInteract(InputAction.CallbackContext context)
     {
-        foreach (Vector3Int coordinate in MapTileManager.TileGridCoordinates)
+        foreach (Vector3Int farmPlotCoordinate in MapTileManager.TileGridCoordinates)
         {
-            if (MapTileManager.PlayerCurrentTile == coordinate)
+            if (MapTileManager.PlayerCurrentTile == farmPlotCoordinate)
             {
-                Debug.Log("Pressed Q, standing on: " + coordinate);
+                Debug.Log("Pressed Q, standing on: " + farmPlotCoordinate);
 
-                FarmPlot targetFarmPlot = MapTileManager.FarmPlots.First(plot => plot.Location == coordinate);
+                FarmPlot targetFarmPlot = MapTileManager.FarmPlots.First(plot => plot.Location == farmPlotCoordinate);
 
                 switch (targetFarmPlot.State)
                 {
@@ -39,9 +39,15 @@ public class FarmInteractions : MonoBehaviour
                         break;
                     case FarmPlotState.Arable:
                         Debug.Log("This plot is arable, anything can be planted here!");
+                        // Remove 1 seed from inventory
+                        Debug.Log("Planting seed...");
+                        // Place seed graphic on farm tile
+                        MapTileManager.PlantSeed(farmPlotCoordinate);
                         break;
                     case FarmPlotState.Growing:
-                        Debug.Log("This plot is growing!");
+                        Debug.Log("This plot is growing...");
+                        MapTileManager.GrowSeed(farmPlotCoordinate);
+                        Debug.Log("The plant is done growing!");
                         break;
                 }
                 
--- CyberDruid/Assets/Scripts/MapScripts/MapTileManager.cs
+++ CyberDruid/Assets/Scripts/MapScripts/MapTileManager.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+
+public class FarmSpriteReference
+{
+    public GameObject seedObject;
+    public Vector3Int plotLocation;
+}
+
 public class MapTileManager : MonoBehaviour
 {
     // Public fields
@@ -9,19 +17,23 @@ public class MapTileManager : MonoBehaviour
     public List<Vector3Int> TileGridCoordinates;
     public List<FarmPlot> FarmPlots;
 
-
     // Inspector fields
     public Tilemap Tilemap;
     public Tile aridSoil;
     public Tile arableSoil;
+    public GameObject seedPrefab;
+    public GameObject growingSeedPrefab;
 
     [SerializeField]
     Transform playerTransform;
 
+    // Private fields
+    List<FarmSpriteReference> PlantSprites;
 
     void Start()
     {
         FarmPlots = new List<FarmPlot>();
+        PlantSprites = new List<FarmSpriteReference>();
         // initialize farm with mix of arable and arid soil
         // Note: This will be pulled from saved player information when in game
         int i = 0;
@@ -55,6 +67,44 @@ public class MapTileManager : MonoBehaviour
     void Update()
     {
         PlayerCurrentTile = Tilemap.WorldToCell(playerTransform.position);
-        Debug.Log("Current tile: " + PlayerCurrentTile);
+    }
+
+    public void PlantSeed(Vector3Int farmPlotCoordinate)
+    {
+        // Find and update target farm plot
+        FarmPlot targetFarmPlot = FarmPlots.First(plot => plot.Location == farmPlotCoordinate);
+
+        // Set farm plot plant to seed type (TODO: specify this when we have more seeds)
+        targetFarmPlot.PlantType = PlantType.FungalBlade;
+
+        // Change farm plot state to growing
+        targetFarmPlot.State = FarmPlotState.Growing;
+
+        // Caclulate world position from tile map coordinate
+        Vector3 centerOfTile = Tilemap.GetCellCenterWorld(farmPlotCoordinate);
+
+        // Place seed sprite at center of farm tile
+        GameObject seedReference = (GameObject) Instantiate(seedPrefab, centerOfTile, Quaternion.identity);
+
+        PlantSprites.Add(new FarmSpriteReference { seedObject = seedReference, plotLocation = farmPlotCoordinate });
+    }
+
+    public void GrowSeed(Vector3Int farmPlotCoordinate)
+    {
+        // Find and update target farm plot
+        FarmPlot targetFarmPlot = FarmPlots.First(plot => plot.Location == farmPlotCoordinate);
+
+        // Change farm plot state to grown
+        targetFarmPlot.State = FarmPlotState.Grown;
+
+        // Caclulate world position from tile map coordinate
+        Vector3 centerOfTile = Tilemap.GetCellCenterWorld(farmPlotCoordinate);
+
+        // Remove old seed sprite, replace with grown seed sprite
+        GameObject oldSprite = PlantSprites.First(s => s.plotLocation == farmPlotCoordinate).seedObject;
+        Destroy(oldSprite);
+
+        // Place seed sprite at center of farm tile
+        Instantiate(growingSeedPrefab, centerOfTile, Quaternion.identity);
     }
 }
